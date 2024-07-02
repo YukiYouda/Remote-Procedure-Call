fb30769e130480a648aa5dc4cdda63ac400e19c6
YukiYouda <youda0518@yahoo.co.jp>
2024-07-02 14:55:31 +0900

    実装完了

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require('net')
+const socketPath = '/tmp/socket_file'
+
+// UNIXドメインソケットを作成
+const client = net.createConnection(socketPath, () => {
+    console.log('connected to server');
+
+    // サーバーにメッセージを送信
+    const message = {
+        method: 'validAnagram',
+        params: ['test','sett'],
+        param_types: ['string', 'string'],
+        id: 2
+    };
+
+    client.write(JSON.stringify(message));
+});
+
+// サーバーからのデータを受信
+client.on('data', (data) => {
+    console.log('Received : ', data.toString());
+    client.end();
+})
+
+// サーバーからの接続終了通知を受信
+client.on('end', () => {
+    console.log('disconnected from server');
+});
---
+++ server.py
@@ -0,0 +1,92 @@
+import socket
+import os
+import json
+import math
+
+def subtract(x, y):
+    return x - y
+
+def floor(x):
+    return math.floor(x)
+
+def nroot(n, x):
+    return math.pow(x, 1/n)
+
+def reverse(s):
+    return s[::-1]
+
+def sort(strArr):
+    return sorted(strArr)
+
+def validAnagram(str1, str2):
+    countMap1 = {}
+    countMap2 = {}
+
+    for char in str1:
+        if char not in countMap1:
+            countMap1[char] = 0
+        countMap1[char] += 1
+
+    for char in str2:
+        if char not in countMap2:
+            countMap2[char] = 0
+        countMap2[char] += 1
+    
+    return countMap1 == countMap2
+
+functions = {
+    'subtract' : subtract, 
+    'floor' : floor,
+    'nroot' : nroot,
+    'reverse' : reverse,
+    'sort' : sort,
+    'validAnagram' : validAnagram,
+}
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+server_address = '/tmp/socket_file'
+
+try:
+    os.unlink(server_address)
+except FileNotFoundError:
+    pass
+
+print('Starting up on {}' .format(server_address))
+
+sock.bind(server_address)
+
+sock.listen(1)
+
+while True:
+    connection, cilent_address = sock.accept()
+    try:
+        print('connection from', cilent_address)
+
+        while True:
+            data = connection.recv(1024)
+            data_json = json.loads(data.decode('utf-8'))
+
+            if data:
+                resonse = {}
+                method = data_json['method']
+                params = data_json['params']
+                param_types = data_json['param_types']
+                id = data_json['id']
+                resonse['result_type'] = param_types[0]
+                resonse['id'] = id
+
+                if method == 'sort':
+                    resonse['results'] = functions[method](params) 
+                elif len(params) == 2:
+                    resonse['results'] = functions[method](params[0], params[1]) 
+                elif len(params) == 1:
+                    resonse['results'] = functions[method](params[0]) 
+                resonse =  json.dumps(resonse).encode('utf-8')
+                connection.sendall(resonse)
+            else:
+                print('no data from', cilent_address)
+                break
+    finally:
+        print('Closing current connection')
+        connection.close()
